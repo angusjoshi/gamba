3097130285fb1aa25c6af53a3705e236692534c7
Angus Joshi <s1712180@ed.ac.uk>
2022-07-22 21:32:27 +0100

    gamba!

---
+++ src/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./Button.css";
+
+export default class Button extends React.Component { 
+
+    render() { 
+        return(
+            <button className="gamba-button" onClick={this.props.onClick}>
+                <p>Gamba!</p>
+            </button>
+        );
+    }
+}
---
+++ src/Casino.js
@@ -0,0 +1,8 @@
+
+export default function makeBet() { 
+    console.log("gamba!");
+    //call some contract method 
+        //contract method should take bet, determine winner, pay out or not,
+        //emit some event indicating if we won or not
+}
+
